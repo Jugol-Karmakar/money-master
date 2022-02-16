0502cd68533924074bab964d2cd0cb2df971e740
JugolKarmakar <jugol.cse@gmail.com>
2022-02-17 05:06:13 +0600

    bank commit

---
+++ js/cost.js
@@ -0,0 +1,22 @@
+function expensesNumber() {
+  const foodInput = document.getElementById("food-field");
+  const foodvalue = parseInt(foodInput.value);
+  const rentInput = document.getElementById("rent-field");
+  const rentValue = parseInt(rentInput.value);
+  const clothInput = document.getElementById("cloth-field");
+  const clothValue = parseInt(clothInput.value);
+
+  const totalCost = foodvalue + rentValue + clothValue;
+  const totalExpenses = document.getElementById("total-expenses");
+  totalExpenses.innerText = totalCost;
+
+  const incomeInput = document.getElementById("income-field");
+  const incomeValue = parseInt(incomeInput.value);
+  const balance = incomeValue - totalCost;
+
+  const totalBalance = document.getElementById("balance");
+  totalBalance.innerText = balance;
+}
+document.getElementById("calculate-btn").addEventListener("click", function () {
+  expensesNumber();
+});
